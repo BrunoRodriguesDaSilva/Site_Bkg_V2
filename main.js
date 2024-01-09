568d795d92e3dee0ba85111447e9c93b71f6e509
Bruno Rodrigues da Silva <Brunochh@outlook.com>
2024-01-09 14:15:47 -0300

    atualização nas imagens e criação do header no arquivo js

---
+++ main.js
@@ -0,0 +1,8 @@
+    var header = document.getElementById("Header");
+    header.innerHTML = '<button><img src="../Imagens/menu.svg" alt=""></button><a href=""><h3>BKG</h3></a><button><img src="../Imagens/sacola-de-compras.svg" alt=""></button>'
+        /*usar Jquery
+        escrever uma funcao para escrever o Header
+        escrever uma funcao para escrever o footer
+        
+        escrever uma funcao para adicionar automaticamente os itens nas paginas dos produtos
+        */
